be05e5a99bfe4214ccb5d5801d51aa202dab3601
Perrine Martyris <perrinemartyris@gmail.com>
2022-02-07 16:57:00 +0100

    add o2switch

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,6 +8,7 @@ const Footer = () => {
     <div className="footer">
       <p>Copyright © 2019, All rights Reserved. <br/>
 Created by Perrine Martyris</p>
+      <a href="https://www.o2switch.fr/" target="_blank"><img src="https://www.o2switch.fr/goodland/7.png" border="0" /></a>
     </div>
   )
   };
