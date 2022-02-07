e07db8a0ca7121d337db97b4172f02c542dff3f8
Perrine Martyris <perrinemartyris@gmail.com>
2022-02-07 19:08:09 +0100

    add the buid folder

---
+++ build/precache-manifest.977de19a9275537c29c6d9483a3e0c28.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8891cda0c92d0c95a3a0528f6aff0b4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b5e807264bc875c946",
+    "url": "/static/css/main.166b6928.chunk.css"
+  },
+  {
+    "revision": "42993aa625cd11390d88",
+    "url": "/static/js/2.ecde4d59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ecde4d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5b5e807264bc875c946",
+    "url": "/static/js/main.cc328669.chunk.js"
+  },
+  {
+    "revision": "78fce1280daaad9c5562",
+    "url": "/static/js/runtime-main.12b0dc2d.js"
+  },
+  {
+    "revision": "282b1be8b82cf45e0877ac02be8efc8e",
+    "url": "/static/media/MonSite.282b1be8.png"
+  },
+  {
+    "revision": "79ef452a08a57bf80fa672e9db92be2d",
+    "url": "/static/media/UpDay.79ef452a.png"
+  },
+  {
+    "revision": "9bc88a85891d27651b9e46acb35ab6d1",
+    "url": "/static/media/UpDayAdmin.9bc88a85.png"
+  },
+  {
+    "revision": "3be5b658075dd379c166c6d2d63aa82d",
+    "url": "/static/media/Yukids.3be5b658.png"
+  },
+  {
+    "revision": "ce740a06399b5822d3ab4354e5f95ebd",
+    "url": "/static/media/bootstrap.ce740a06.svg"
+  },
+  {
+    "revision": "371c920ba10a46409479a49bd9cfc571",
+    "url": "/static/media/css3.371c920b.svg"
+  },
+  {
+    "revision": "9e82f6f3096f3e3a39dbdad6f263ae56",
+    "url": "/static/media/firebase.9e82f6f3.svg"
+  },
+  {
+    "revision": "a3e0c08911de92899862f22680e4e8f5",
+    "url": "/static/media/git.a3e0c089.svg"
+  },
+  {
+    "revision": "be41af3a263a54d9e8b396d44cb03e8e",
+    "url": "/static/media/github.be41af3a.svg"
+  },
+  {
+    "revision": "23e2d9ce53c624499f8ad71bd24367cb",
+    "url": "/static/media/html.23e2d9ce.svg"
+  },
+  {
+    "revision": "59e6100d1b737aa88cf6f39bb0eba565",
+    "url": "/static/media/illustrator.59e6100d.svg"
+  },
+  {
+    "revision": "9d4131495393d4a1f58a40bc5cdb57bf",
+    "url": "/static/media/indesign.9d413149.svg"
+  },
+  {
+    "revision": "3f9ca62fc3b9eb791c709f59c0648d07",
+    "url": "/static/media/javascript.3f9ca62f.svg"
+  },
+  {
+    "revision": "dc2e56656307c5859b5bc7c00ad6fd3c",
+    "url": "/static/media/myPict.dc2e5665.jpg"
+  },
+  {
+    "revision": "35367062db126c22b2f1dd6b0b90dd63",
+    "url": "/static/media/mySql.35367062.svg"
+  },
+  {
+    "revision": "a1f7b38fecbcef723829df8d9bb14fb6",
+    "url": "/static/media/node.a1f7b38f.svg"
+  },
+  {
+    "revision": "a26cab3aa8ed66a8b42560c93d89d155",
+    "url": "/static/media/photoshop.a26cab3a.svg"
+  },
+  {
+    "revision": "45e46bc025b8fd350c09492ce01d4549",
+    "url": "/static/media/postgresql.45e46bc0.svg"
+  },
+  {
+    "revision": "1a2e6932e63ee4637912ab6d92eccce5",
+    "url": "/static/media/react.1a2e6932.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "a5ab8a67d52562b26d6c9dbef5b7d423",
+    "url": "/static/media/sass.a5ab8a67.svg"
+  },
+  {
+    "revision": "ec1d6059b2d2d941f96d0ba248363342",
+    "url": "/static/media/scrum.ec1d6059.svg"
+  },
+  {
+    "revision": "43c9d125de48e59cb29a88dd1b20bc46",
+    "url": "/static/media/sequelize.43c9d125.svg"
+  },
+  {
+    "revision": "7a1a193f22e35d41fff0d12b96f1da2c",
+    "url": "/static/media/surfboardOnTheBeach.7a1a193f.jpg"
+  },
+  {
+    "revision": "a232bac60396d5539d8de360119d6685",
+    "url": "/static/media/trello.a232bac6.svg"
+  },
+  {
+    "revision": "e00a35696e2159961e17ae487576275e",
+    "url": "/static/media/vsCode.e00a3569.svg"
+  }
+]);
