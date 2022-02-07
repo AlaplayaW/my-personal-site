9a88efedfc8ab87668919ce59166534ff91a6c80
Perrine Martyris <perrinemartyris@gmail.com>
2022-02-07 19:23:23 +0100

    hope to resolve problem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="footer">
       <p>Copyright © 2019, All rights Reserved. <br/>
 Created by Perrine Martyris</p>
-      <a class="o2switch-link" href="https://www.o2switch.fr/" target="_blank"><img class="o2switch-logo" src="https://www.o2switch.fr/goodland/7.png" border="0" /></a>
+      <a class="o2switch-link" href="https://www.o2switch.fr/" target="_blank" rel="noopener noreferrer"><img class="o2switch-logo" src="https://www.o2switch.fr/goodland/7.png" border="0" alt="o2switch logo"/></a>
     </div>
   )
   };
