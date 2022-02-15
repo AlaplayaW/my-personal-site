bc12198873747eeec0c37a2107f2198543374c26
Perrine Martyris <perrinemartyris@gmail.com>
2022-02-15 16:33:08 +0100

    add the affiliate program with o2switch link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,7 @@ const Footer = () => {
     <div className="footer">
       <p>Copyright © 2019, All rights Reserved. <br/>
 Created by Perrine Martyris</p>
-      <a href="https://www.awin1.com/cread.php?awinmid=13324&awinaffid=1020721&clickref=o2switch&ued=https%3A%2F%2Fwww.o2switch.fr%2F" target="_blank" rel="sponsored noopener noreferrer"><img class="o2switch-logo" src="https://www.o2switch.fr/goodland/7.png" border="0" alt="o2switch logo"/></a>
-      <a class="o2switch-link" href="https://www.o2switch.fr/" target="_blank" rel="noopener noreferrer">o2switch</a>
+      <a class="o2switch-link" href="https://www.awin1.com/cread.php?awinmid=13324&awinaffid=1020721&clickref=o2switch&ued=https%3A%2F%2Fwww.o2switch.fr%2F" target="_blank" rel="sponsored noopener noreferrer">o2switch</a>
     </div>
   )
   };
